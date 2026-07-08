37cc521c881b32fd8bf8130ce108ff1469b0b948
sahasagnik022-cmyk <sahasagnik022@gmail.com>
2026-07-08 22:42:47 +0530

    Update ram_test.sv

--- testbench/ram_test.sv
+++ testbench/ram_test.sv
@@ -1,5 +1,5 @@
 class ram_test;
-    virtual ram_if vif; 
+    virtual ram_if vif;
     ram_environment env;
     function new(virtual ram_if vif);
         this.vif=vif;
@@ -12,18 +12,97 @@ class ram_test;
     endtask
 endclass
 
-class ram_test_negative extends ram_test;
+class ram_test_directed extends ram_test;
+
     function new(virtual ram_if vif);
-        super.new(vif); 
+        super.new(vif);
     endfunction
+
     task run();
-        $display("[%0t] [TST] ILLEGAL Negative Test Started.", $time);
+        $display("--- Starting Directed Sequence Test ---");
         env = new(vif);
         env.build();
-        env.gen.blueprint = new();
-        env.gen.blueprint.wr_rd.constraint_mode(0);
-        env.gen.blueprint.randomize() with { write_enb == 1; read_enb == 1; };
-        env.run();
+        fork
+            env.drv.run();
+            env.mon.run();
+            env.ref_model.run();
+            env.scb.run();
+        join_none
+        wait(vif.reset == 1);
+        repeat(1)@(posedge vif.clk)
+        env.gen.write_sequence(5);
+        repeat(5)@(posedge vif.clk)
+        env.gen.hold_sequence(2);
+        repeat(2)@(posedge vif.clk)
+        env.gen.read_sequence(5);
+        repeat(5)@(posedge vif.clk)
+        $finish;
     endtask
-    
+endclass
+
+
+class ram_test_reset extends ram_test;
+
+    function new(virtual ram_if vif);
+        super.new(vif);
+    endfunction
+
+    task run();
+        ram_transaction tx;
+
+        env = new(vif);
+        env.build();
+
+        fork
+            env.drv.run();
+            env.mon.run();
+            env.ref_model.run();
+            env.scb.run();
+        join_none
+
+        wait(vif.reset == 1);
+        @(posedge vif.clk);
+
+        tx = new();
+        tx.write_enb = 1;
+        tx.read_enb  = 0;
+        tx.address   = 5;
+        tx.data_in   = 42;
+        env.gen.mbx.put(tx);
+
+        repeat(2) @(posedge vif.clk);
+
+        tx = new();
+        tx.write_enb = 1;
+        tx.read_enb  = 0;
+        tx.address   = 10;
+        tx.data_in   = 99;
+        env.gen.mbx.put(tx);
+
+        @(posedge vif.clk);
+        -> vif.trig_rst;
+
+        wait(vif.reset == 0);
+        wait(vif.reset == 1);
+        repeat(2) @(posedge vif.clk);
+
+        tx = new();
+        tx.write_enb = 0;
+        tx.read_enb  = 1;
+        tx.address   = 10;
+        env.gen.mbx.put(tx);
+
+        repeat(2) @(posedge vif.clk);
+
+        tx = new();
+        tx.write_enb = 0;
+        tx.read_enb  = 1;
+        tx.address   = 5;
+        env.gen.mbx.put(tx);
+
+        repeat(5) @(posedge vif.clk);
+
+        $finish;
+    endtask
+
 endclass
